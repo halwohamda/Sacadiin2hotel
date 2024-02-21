212b5fa6a007cfab0450964d6d486a324be6469a
halwohamda <halwohamda@gmail.com>
2024-02-20 19:49:36 -0800

    done

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import Get from './Components/Hotel/Get.jsx'
+import Post from './Components/Hotel/Post.jsx'
+import Update from './Components/Hotel/Update.jsx'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    children:[
+      {
+        path:'/getAll',
+        element: <Get />
+      },
+      {
+        path:'/Post',
+        element: <Post />
+      },
+      {
+        path:'/Update',
+        element: <Update />
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+    
   </React.StrictMode>,
 )
