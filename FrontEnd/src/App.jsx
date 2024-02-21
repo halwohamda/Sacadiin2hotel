e6211ce67e218676c7bafec61ee7f43d6d72701a
halwohamda <halwohamda@gmail.com>
2024-02-20 19:39:14 -0800

    done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-
-const  App = () => {
+import {Link,Outlet} from 'react-router-dom'
+const App = () => {
   return (
     <div>
-
-
-      
+      <div className="header bg-blue-600 p-4 text-center font-bold text-3xl text-white uppercase ">
+       <Link to='/'><h1>Sacaada Hotel</h1></Link>
+      </div>
+      <h1 className='text-center '>Welcome to DashBoard</h1>
+      <div className="menu">
+        <nav>
+          <ul>
+            <Link to='/GetAll'>Hotel</Link>
+          </ul>
+        </nav>
+      </div>
+      <Outlet />
     </div>
   )
 }
 
-export default  App
+export default App
