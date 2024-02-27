89ca81c640352d95b294e2273bfe3428a44cb2f5
halwohamda <halwohamda@gmail.com>
2024-02-26 17:45:15 -0800

    done

--- BackEnd/routes/EmployeeRoute.js
+++ BackEnd/routes/EmployeeRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = require ('router');
 const EmployeController = require('../Controllers/EmployeeController')
-router.get('all/',EmployeController.getAll);
+router.get('/all',EmployeController.getAll);
+router.Post('/new',EmployeController.PostEmployee);
